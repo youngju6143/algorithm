6c7f729f2ef048fbbdb41a246f710e7d1421d04d
영주 <111039206+youngju6143@users.noreply.github.com>
2024-11-21 10:27:31 +0900

    [Silver II] Title: 유기농 배추, Time: 0 ms, Memory: 2040 KB -BaekjoonHub

--- 백준/Silver/1012. 유기농 배추/유기농 배추.cc
+++ 백준/Silver/1012. 유기농 배추/유기농 배추.cc
@@ -4,48 +4,58 @@
 using namespace std;
 #define endl '\n'
 
-int graph[51][51] = {0, };
-int visited[51][51] = {0, }; // 방문 여부 확인
-int xpos[4] = {0, 0, -1, 1}; // x좌표 이동
-int ypos[4] = {-1, 1, 0, 0}; // y좌표 이동
-int t, m, n, k, x, y;
+int dx[4] = {0, 0, -1, 1};
+int dy[4] = {-1, 1, 0, 0};
+
+int graph[51][51] = {0, }; // 그래프
+int visited[51][51] = {0, }; // 방문 여부
+int t, m, n, k;
 
 void dfs(int x, int y) {
     for (int i = 0; i < 4; i++) {
-        int currentX = x + xpos[i]; // 상하좌우 탐색
-        int currentY = y + ypos[i]; // 상하좌우 탐색
-        if (currentX < 0 || currentY < 0 || currentX >= m || currentY >= n) // 그래프 범위 넘어서면
+        int nextX = x + dx[i];
+        int nextY = y + dy[i]; 
+
+        // 그래프 범위를 벗어나거나 이미 방문한 경우 무시
+        if (nextX < 0 || nextY < 0 || nextX >= m || nextY >= n)
             continue;
-        if (graph[currentX][currentY] == 1 && !visited[currentX][currentY]) { // 배추가 있고, 한 번도 방문하지 않았다면
-            visited[currentX][currentY] = 1;
-            dfs(currentX, currentY); // 재귀 ㄱㄱ
+
+        // 배추가 있고 방문하지 않은 경우
+        if (graph[nextX][nextY] == 1 && !visited[nextX][nextY]) {
+            visited[nextX][nextY] = 1;
+            dfs(nextX, nextY);
         }
     }
 }
 
 int main() {
     cin.tie(NULL)->ios::sync_with_stdio(false);
-    
+
     cin >> t;
-    for (int i = 0; i < t; i++) {
-        int cnt = 0;
+    while (t--) {
         cin >> m >> n >> k;
-        for (int j = 0; j < k; j++) {
+
+        memset(graph, 0, sizeof(graph));
+        memset(visited, 0, sizeof(visited));
+
+        for (int i = 0; i < k; i++) {
+            int x, y;
             cin >> x >> y;
-            graph[x][y] = 1; // 해당 그래프 좌표에 배추 넣기
+            graph[x][y] = 1;
         }
-        for (int j = 0; j < m; j++) {
-            for (int l = 0; l < n; l++) {
-                if (graph[j][l] == 1 && !visited[j][l]) { // 배추가 있고, 한 번도 방문하지 않았다면
-                    cnt++; // 배추흰지렁이 개수 추가
-                    dfs(j, l); // dfs 진행
+
+        int wormCount = 0;
+        for (int x = 0; x < m; x++) {
+            for (int y = 0; y < n; y++) {
+                if (graph[x][y] == 1 && !visited[x][y]) {
+                    wormCount++;
+                    visited[x][y] = 1;
+                    dfs(x, y);
                 }
             }
         }
-        cout << cnt << endl;
-        memset(graph, 0, sizeof(graph)); // graph 0으로 초기화
-        memset(visited, 0, sizeof(visited)); // visited 0으로 초기화
+        cout << wormCount << endl;
     }
+
     return 0;
 }
-
