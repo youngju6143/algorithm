1b494d09d0e7489163915c447cb101c03c677e69
영주 <youngju6143@khu.ac.kr>
2026-03-17 23:00:15 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 42.92 ms, Memory: 54.1 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/README.md
+++ 프로그래머스/1/42576. 완주하지 못한 선수/README.md
@@ -1,10 +1,10 @@
 # [level 1] 완주하지 못한 선수 - 42576 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42576) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42576?language=javascript) 
 
 ### 성능 요약
 
-메모리: 69.3 MB, 시간: 34.66 ms
+메모리: 54.1 MB, 시간: 42.92 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 07월 27일 18:45:34
+2026년 03월 17일 23:00:12
 
 ### 문제 설명
 
--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.js
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.js
@@ -1,10 +1,4 @@
 function solution(participant, completion) {
-  var table = {};
-  var answer = "";
-  for (let i = 0; i < participant.length; i++) table[participant[i]] = 0;
-  for (let i = 0; i < participant.length; i++) table[participant[i]]++;
-  for (let i = 0; i < completion.length; i++) table[completion[i]]--;
-
-  for (let i = 0; i < participant.length; i++)
-    if (table[participant[i]] != 0) return participant[i];
+    var answer = '';
+    return answer;
 }
